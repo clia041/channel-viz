b45b7e3affa3f03900136c3e2c730f67f200353d
clia041 <clia041@users.noreply.github.com>
2014-08-03 15:43:23 +1200

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,18 +1,25 @@
-function ( $ ){
+(function ( $ ){
   "use strict";
-  var feedID = 1868918819;
+
+  var feedID = 79650;
+
+  // SET API KEY
   
-  xively.setKey("ywafB6MNLUakqggSfAgfrSaQoyRhPsbYbRVfuqVILy5ctgQr");
-  
-  xively.feed.get(feedID, function(data)){
-    
+  xively.setKey( "FzZOVUxKRPl_Tvvtr1z77b8gOVCSAKxlTnlwSzQrZ3U4MD0g" ); // do not use this one, create your own at xively.com
+
+  // get all feed data in one shot
+
+  xively.feed.get (feedID, function (data) {
+    // this code is executed when we get data back from Xively
+
     var feed = data,
         datastream,
         value,
-        handleToggle = function(datastreamID, value) {
-          var $toggle = $(".js-"+datastreamID+"toggle");
-          
-           if ( value ) {
+        // function for setting up the toggle inputs
+        handleToggle = function ( datastreamID, value ) {
+          var $toggle = $(".js-"+ datastreamID +"-toggle");
+
+          if ( value ) {
             // lights are on
             ui.toggle.on( $toggle, true );
           }
@@ -20,7 +27,8 @@ function ( $ ){
             // lights are off
             ui.toggle.off( $toggle, true );
           }
-                    // save changes
+
+          // save changes
           $(".js-"+ datastreamID).on("change", function(){
             $(".app-state").addClass("loading").fadeIn(200);
 
@@ -56,4 +64,72 @@ function ( $ ){
     for (var x = 0, len = feed.datastreams.length; x < len; x++) {
       datastream = feed.datastreams[x];
       value = parseInt(datastream["current_value"]);
-  }
+
+      // LIGHTS
+
+      if ( datastream.id === "lights" ) {
+        handleToggle( "lights", value );
+      }
+
+      // TV
+
+      if ( datastream.id === "tv-state" ) {
+        handleToggle( "tv-state", value );
+      }
+
+      // MUSIC
+
+      if ( datastream.id === "volume" ) {
+        var $range = $(".js-volume");
+        
+        // set value
+        $range.val(value);
+
+        // save changes
+        $range.on("custom-change", function( event, val ) {
+          $(".app-state").addClass("loading").fadeIn(200);
+          xively.datastream.update(feedID, "volume", { "current_value": val }, function(){
+            $(".app-state").removeClass("loading").fadeOut(200);
+          });
+        });
+
+        // make it live
+        xively.datastream.subscribe(feedID, "volume", function ( event, data ) {
+          ui.fakeLoad();
+          $range.val(parseInt(data["current_value"]));
+        });
+
+      }      
+
+      // TEMPERATURE
+
+      if ( datastream.id === "temperature" ) {
+        var $temperature = $(".js-temperature");
+
+        $temperature.html( datastream["current_value"] );
+
+        // save changes
+        $temperature.on("custom-change", function( event, val ) {
+          $(".app-state").addClass("loading").fadeIn(200);
+          xively.datastream.update(feedID, "temperature", { "current_value": val }, function(){
+            $(".app-state").removeClass("loading").fadeOut(200);
+          });
+        });
+
+        // make it live
+        xively.datastream.subscribe( feedID, "temperature", function ( event , data ) {
+          ui.fakeLoad();
+          $temperature.html( data["current_value"] );
+        });
+      }
+    }
+
+    // SHOW UI
+
+    $(".app-loading").fadeOut(200, function(){
+     $(".app-content-inner").addClass("open");
+    });
+  });
+
+
+})( jQuery );
