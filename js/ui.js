907bdda66cb7ff3fa1aaef4c821d7676a459131b
clia041 <clia041@users.noreply.github.com>
2014-08-16 13:52:06 +1200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -53,5 +53,61 @@ var ui      = {};
     };
   })();
 
+  // UI RANGE
+
+  if ( Modernizr.inputtypes.range ) {
+    // custom change event
+    $("[data-range]").on("mouseup touchend", function( event ) {
+      var $this = $( event.target );
+      $this.trigger( "custom-change", $this.val() );
+    });
+  }
+
+  // fake load to help user detect realtime changes
+  ui.fakeLoad = function () {
+    $(".app-state").addClass(".loading").fadeIn(200,function(){
+      $(".app-state").removeClass("loading").fadeOut(200);
+    });
+  }
+
+  var handleVolume = function ( event ) {
+    var $range    = $(".js-volume"),
+        increase  = ( $(event.target).hasClass("js-volume-up") ) ? true : false,
+        curVal    = parseInt($range.val()),
+        targetVal = ( increase ) ? curVal + 10 : curVal - 10 ;
+
+    event.preventDefault();
+
+    if ( $(".app-state").hasClass("loading") ) { return false; }
+
+    if ( targetVal > 100 ) {
+      targetVal = 100;
+    }
+    else if ( targetVal < 0 ) {
+      targetVal = 0;
+    }
+
+    if ( curVal == targetVal ) { return false; }
+
+    $range.val(targetVal).trigger("custom-change", targetVal);
+  };
+
+  $(".js-volume-up").on("click", handleVolume);
+  $(".js-volume-down").on("click", handleVolume);
+
+  var handleTemp = function ( event ) {
+    var $temperature  = $(".js-temperature"),
+        increase      = ( $(event.target).hasClass("js-temp-up") ) ? true : false,
+        curVal        = parseInt($temperature.html()),
+        targetVal     = ( increase ) ? curVal + 1 : curVal - 1 ;
+
+    event.preventDefault();
+    if ( $(".app-state").hasClass("loading") || curVal == targetVal ) { return false; }
+
+    $temperature.html(targetVal).trigger("custom-change", targetVal);
+  }
+
+  $(".js-temp-up").on("click", handleTemp);
+  $(".js-temp-down").on("click", handleTemp);
 
 })( jQuery );
